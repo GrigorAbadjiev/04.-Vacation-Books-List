d640a724629fc7cb807ef6921f3306c223d12667
nanji <nanji@DESKTOP-4A8MIDK>
2022-10-30 12:25:26 +0200

    Add project files.

---
+++ 04. Vacation Books List/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04._Vacation_Books_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            int pageCount = int.Parse(Console.ReadLine());
+            int pageReadForOneHouer = int.Parse(Console.ReadLine());
+            int dayToReadTheBook = int.Parse(Console.ReadLine());
+            // calculate
+            int houresToRedTheBook = pageCount / pageReadForOneHouer;
+            int daysRequiredToReadtheBook = houresToRedTheBook / dayToReadTheBook; 
+            // output
+          
+            Console.WriteLine(daysRequiredToReadtheBook);
+
+           
+        }
+    }
+}
